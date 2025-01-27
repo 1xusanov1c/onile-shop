f2ddac7bef679740cc46e61c352ca6da2feb5830
1xusanov1c <azz806@gmail.com>
2025-01-27 21:01:56 +0500

    rasmlani URL ni togrlab chqdim

--- js/products.js
+++ js/products.js
@@ -4,7 +4,7 @@ let products = [
         product:
           "Fitbit Sense Advanced Smartwatch with Tools for Heart Health, Stress Management & Skin Temperature Trends, Carbon/Graphite, One Size (S & L Bands)",
         productImage:
-          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3LlA5-3zBq2Zholkq5EzdZKK0lU9ENfQJww&s",
         color: "Pure matte black",
         price: 57,
         size: 42,
@@ -14,7 +14,7 @@ let products = [
         id: 2,
         product: "2021 Apple 12.9-inch iPad Pro (Wi‑Fi, 128GB) - Space Gray",
         productImage:
-          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGxpmDP_C7_4f4MIR0gwEpLo9D9wkltFLLTA&s",
         color: "Black",
         price: 1499,
         size: "Pro",
@@ -24,7 +24,7 @@ let products = [
         id: 3,
         product: "PlayStation 5 DualSense Wireless Controller",
         productImage:
-          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+          "https://m.media-amazon.com/images/I/41ECK5cY-2L.jpg",
         color: "White",
         price: 299,
         size: "Regular",
@@ -34,7 +34,7 @@ let products = [
         id: 4,
         product: "Apple MacBook Pro 13 inch-M1-8/256GB-space",
         productImage:
-          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6oXitbilm-XuOwAB9voCrgVsqBoSJf1FM-Q&s",
         color: "Space Gray",
         price: 1699,
         size: "Pro",
@@ -45,7 +45,7 @@ let products = [
         product:
           'Apple iMac 24" 4K Retina Display M1 8 Core CPU, 7 Core GPU, 256GB SSD, Green (MJV83ZP/A) 2021',
         productImage:
-          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROJibxPoCL_xZEHhsix8Xvah3FoOv2D4tKYg&s",
         color: "Ocean Blue",
         price: 65,
         size: '21"',
@@ -55,7 +55,7 @@ let products = [
         id: 6,
         product: "Apple Magic Mouse (Wireless, Rechargable) - Silver",
         productImage:
-          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+          "https://sa.newtechstore.com/cdn/shop/products/apple-magic-mouse-2-wireless-and-rechargable-mouse-apple-silver-510209_250x.jpg?v=1629176805",
         color: "White",
         price: 30,
         size: "Regular",
@@ -65,7 +65,7 @@ let products = [
         id: 7,
         product: "Amazon Basics Matte Black Wired Keyboard - US Layout (QWERTY)",
         productImage:
-          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+          "https://m.media-amazon.com/images/I/715V97qcv2L._AC_UF894,1000_QL80_.jpg",
         color: "Black",
         price: 40,
         size: "MD",
@@ -75,7 +75,7 @@ let products = [
         id: 8,
         product: "HORI Racing Wheel Apex for PlayStation 4_3, and PC",
         productImage:
-          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs1yuFzVvmJDPAPPFzCsOPPH3wx50ZINSzDw&s",
         color: "Black",
         price: 130,
         size: "45",
@@ -85,7 +85,7 @@ let products = [
         id: 9,
         product: "Xbox Series S",
         productImage:
-          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeVsaYao6PN3i1v9lhOnackCbo_7ZnppEiww&s",
         color: "Space Gray",
         price: 99,
         size: "sm",
