c878b022fdee9a072d0bbcf648007e28f3e54ba0
1xusanov1c <azz806@gmail.com>
2025-01-27 20:45:59 +0500

    ishni tugatim

---
+++ js/main.js
@@ -0,0 +1,117 @@
+let cards = document.querySelector('#cards')
+let count = document.querySelector('#count')
+let product = document.querySelector('#product')
+let productImage = document.querySelector('#productImage')
+let price = document.querySelector('#price')
+let quantity = document.querySelector('#quantity')
+let input = document.querySelector('#input')
+
+function renderCard(){
+    cards.innerHTML = null
+    products.forEach((prod, i )=>{
+        let card_div = document.createElement('div')
+        card_div.classList.add('gap-[15px]', 'border' , 'rounded-lg')
+
+        let images = document.createElement('img')
+        images.classList.add('w-[300px' , 'rounded-lg')
+        images.src = prod.productImage
+        images.alt = prod.product
+
+        let cards_div = document.createElement('div')
+        cards_div.classList.add('py-1', 'px-3')
+
+        let nomi = document.createElement('p')
+        nomi.textContent = prod.product.length>= 100 ? `${prod.product.slice(0, 100) + "..."}`: prod.product
+
+        let soni = document.createElement('p')
+        soni.classList.add('font-bold')
+        soni.textContent = "Mahsulot soni: " + prod.quantity;
+
+        let narxi = document.createElement('p')
+        narxi.classList.add('font-bold')
+        narxi.textContent = "Mahsulot narxi: " + prod.price
+
+        let button = document.createElement('button')
+        button.classList.add('bg-blue-600', 'py-1', 'px-3', 'active:bg-blue-400', 'rounded-lg', 'text-white')
+        button.textContent = "Sotib olish "
+
+        button.setAttribute('onclick' , `sotibOlish(${i})`)
+
+
+        
+        cards_div.appendChild(nomi)
+        cards_div.appendChild(soni)
+        cards_div.appendChild(narxi)
+        cards_div.appendChild(button)
+        
+        card_div.appendChild(images)
+        card_div.appendChild(cards_div)
+
+        cards.appendChild(card_div)
+        
+    })
+}
+
+renderCard()
+
+
+let savatcha = []
+
+function sotibOlish(i){
+    if(savatcha.length == 0){
+        let new_prod = {...products[i] , count: 1};
+        savatcha.push(new_prod);
+    }else{
+        let finded = savatcha.find((item) => {
+            return item.id == products[i].id;
+        });
+        if(!finded){
+            let new_prod = {...products[i] , count :1};
+            savatcha.push(new_prod);
+        }else{
+            finded.count += 1;
+        }
+    }
+
+    products[i].quantity -= 1;
+    if(products[i].quantity == 0){
+        products.splice(i , 1)
+    }
+
+    renderCard()
+    count.textContent = savatcha.length
+
+}
+
+function btnApp(){
+    input.classList.remove('hidden')
+    cards.classList.add('hidden')
+}
+
+function appBtn(){
+    try {
+        if(!product.value) throw Error ("Mahsulot nomni kirting")
+        if(!productImage.value) throw Error ("Mahsulot rasmni kirting")
+        if(!price.value) throw Error ("Mahsulot narxini kirting")
+        if(!quantity.value) throw Error ("Mahsulot sonini kirting")
+
+
+        let newProduct = new Addbtn( product.value , productImage.value , price.value , quantity.value)
+        products.push(newProduct)
+        renderCard()
+        cards.classList.remove('hidden')
+        input.classList.add('hidden')
+
+    } catch (error) {
+        alert(error.message)
+    }
+
+}
+
+
+function Addbtn(_product, _productImage, _price, _quantity){
+    this.product = _product
+    this.productImage = _productImage
+    this.price = _price
+    this.quantity = _quantity
+}
---
+++ js/products.js
@@ -0,0 +1,95 @@
+let products = [
+    {
+        id: 1,
+        product:
+          "Fitbit Sense Advanced Smartwatch with Tools for Heart Health, Stress Management & Skin Temperature Trends, Carbon/Graphite, One Size (S & L Bands)",
+        productImage:
+          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+        color: "Pure matte black",
+        price: 57,
+        size: 42,
+        quantity: 4,
+      },
+      {
+        id: 2,
+        product: "2021 Apple 12.9-inch iPad Pro (Wi‑Fi, 128GB) - Space Gray",
+        productImage:
+          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+        color: "Black",
+        price: 1499,
+        size: "Pro",
+        quantity: 2,
+      },
+      {
+        id: 3,
+        product: "PlayStation 5 DualSense Wireless Controller",
+        productImage:
+          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+        color: "White",
+        price: 299,
+        size: "Regular",
+        quantity: 2,
+      },
+      {
+        id: 4,
+        product: "Apple MacBook Pro 13 inch-M1-8/256GB-space",
+        productImage:
+          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+        color: "Space Gray",
+        price: 1699,
+        size: "Pro",
+        quantity: 2,
+      },
+      {
+        id: 5,
+        product:
+          'Apple iMac 24" 4K Retina Display M1 8 Core CPU, 7 Core GPU, 256GB SSD, Green (MJV83ZP/A) 2021',
+        productImage:
+          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+        color: "Ocean Blue",
+        price: 65,
+        size: '21"',
+        quantity: 1,
+      },
+      {
+        id: 6,
+        product: "Apple Magic Mouse (Wireless, Rechargable) - Silver",
+        productImage:
+          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+        color: "White",
+        price: 30,
+        size: "Regular",
+        quantity: 3,
+      },
+      {
+        id: 7,
+        product: "Amazon Basics Matte Black Wired Keyboard - US Layout (QWERTY)",
+        productImage:
+          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+        color: "Black",
+        price: 40,
+        size: "MD",
+        quantity: 2,
+      },
+      {
+        id: 8,
+        product: "HORI Racing Wheel Apex for PlayStation 4_3, and PC",
+        productImage:
+          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+        color: "Black",
+        price: 130,
+        size: "45",
+        quantity: 1,
+      },
+      {
+        id: 9,
+        product: "Xbox Series S",
+        productImage:
+          "https://storage.kun.uz/source/5/Zxr9IEut1Mqxb2t9qafU7K2DHOHEcZqY.jpg",
+        color: "Space Gray",
+        price: 99,
+        size: "sm",
+        quantity: 1,
+      },
+    ];
+    
